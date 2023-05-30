04fe0930a4f2a7a17479200b6c4ba09f068c2ca4
ysmoradi <ysmoradi@DESKTOP-78E6E6P>
2023-05-30 17:23:50 +0330

    Add project files.

---
+++ MudBlazor.Benchmark/Client/Program.cs
@@ -0,0 +1,13 @@
+using MudBlazor.Benchmark.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services.AddMudServices();
+
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+await builder.Build().RunAsync();
