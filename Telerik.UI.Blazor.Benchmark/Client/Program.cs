7c02183db89abb7248ad94bdc814d81987bddbc2
Milad Karimifard <46561982+milad-karimifard@users.noreply.github.com>
2023-06-14 11:55:38 +0330

    Add `Telerik` UI kit benchmark (#1)
    * Add project structure
    
    * add 5 example component
    
    * remove rider files

---
+++ Telerik.UI.Blazor.Benchmark/Client/Program.cs
@@ -0,0 +1,12 @@
+using Telerik.UI.Blazor.Benchmark.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddTelerikBlazor();
+
+await builder.Build().RunAsync();
