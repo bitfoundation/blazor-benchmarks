346533a6cf8d390c92c3b7e79d9848a0e1fac52c
Milad Karimifard <46561982+milad-karimifard@users.noreply.github.com>
2023-08-09 17:13:45 +0330

    Add pure blazor template (#8)

---
+++ Pure.Blazor.Benchmark/Client/Program.cs
@@ -0,0 +1,10 @@
+using Pure.Blazor.Benchmark.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+await builder.Build().RunAsync();
