fd50ab308c2929c73b3ee5146cad3db96af1e0ce
Milad Karimifard <karimifard.milad@gmail.com>
2023-08-11 19:37:01 +0330

    add mat blazor sample

---
+++ MatBlazor.Benchmark/Client/Program.cs
@@ -0,0 +1,13 @@
+using MatBlazor;
+using MatBlazor.Benchmark.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddMatBlazor();
+
+await builder.Build().RunAsync();
