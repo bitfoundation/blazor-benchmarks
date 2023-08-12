406473f78db15cafbbee0603994f6b32b8e38d7a
Milad Karimifard <46561982+milad-karimifard@users.noreply.github.com>
2023-08-12 21:13:38 +0330

    add Syncfusion sample (#12)

---
+++ Syncfusion.Blazor.Benchmark/Client/Program.cs
@@ -0,0 +1,13 @@
+using Syncfusion.Blazor.Benchmark.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Syncfusion.Blazor;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddSyncfusionBlazor();
+
+await builder.Build().RunAsync();
